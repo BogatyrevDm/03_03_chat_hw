f81e3147dbc47168257639b3e29a907adfaf1c50
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-06-05 08:04:44 +0300

    task 4

--- task_4.py
+++ task_4.py
@@ -1,3 +1,17 @@
 """
-4. Преобразовать слова «разработка», «администрирование», «protocol», «standard» из строкового представления в байтовое и выполнить обратное преобразование (используя методы encode и decode).
+4. Преобразовать слова «разработка», «администрирование», «protocol», «standard» из строкового представления
+в байтовое и выполнить обратное преобразование (используя методы encode и decode).
 """
+
+list_str = ["разработка", "администрирование", "protocol", "standard"]
+list_uni = []
+list_decode = []
+
+for i in list_str:
+    list_uni.append(i.encode('utf-8'))
+print(list_uni)
+
+for i in list_uni:
+    list_decode.append(i.decode('utf-8'))
+
+print(list_decode)
