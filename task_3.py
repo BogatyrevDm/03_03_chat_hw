55a9dd9e3ba74d188c4b6feee3f5a8e513b8a179
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-06-05 07:44:21 +0300

    task 3

--- task_3.py
+++ task_3.py
@@ -1,3 +1,10 @@
 """
 3. Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе.
 """
+
+list_byte = ['attribute', 'класс', 'функция', 'type']
+for i in list_byte:
+    try:
+        bytes(i, 'ascii')
+    except UnicodeEncodeError:
+        print(f'You can\'t encode word {i} into bytes!')
