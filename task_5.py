9a2c6109dc96493f738d9471fe5eedb7c567983d
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-06-06 07:10:07 +0300

    task 5

--- task_5.py
+++ task_5.py
@@ -1,3 +1,15 @@
 """
 5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в строковый тип на кириллице.
 """
+
+import subprocess
+import chardet
+args = ['ping', 'yandex.ru']
+
+subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+
+for line in subproc_ping.stdout:
+    result = chardet.detect(line)
+    print(result)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
