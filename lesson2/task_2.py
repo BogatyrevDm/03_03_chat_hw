d02fdf010410adda2581c771cc7bdf4dcab5763f
Dmitrii Bogatyrev <BogatyrevDm82@gmail.com>
2023-06-22 20:15:30 +0300

    task 2

--- lesson2/task_2.py
+++ lesson2/task_2.py
@@ -9,3 +9,25 @@ a. Создать функцию write_order_to_json(), в которую пер
 b. Проверить работу программы через вызов функции write_order_to_json() с передачей
 в нее значений каждого параметра.
 """
+import json
+
+
+def write_order_to_json(item, quantity, price, buyer, date):
+    with open('orders.json', 'r', encoding='utf-8') as f:
+        objs = json.load(f)
+        orders = objs['orders']
+
+    with open('orders.json', 'w', encoding='utf-8') as f:
+        content_to_file = {
+            'item': item,
+            'quantity': quantity,
+            'price': price,
+            'buyer': buyer,
+            'date': date
+        }
+        orders.append(content_to_file)
+        json.dump(objs, f, indent=4, ensure_ascii=False)
+
+
+write_order_to_json('Сервер', 10, 15, 'Sergeev S.', '25/11/1995')
+write_order_to_json('Сервер', 100, 150, 'Ivanov', '25/11/2005')
