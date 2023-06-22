b9204854ac7648bb9525257138ca51dc57eebce6
BogatyrevDm <bogatyrevdm82@gmail.com>
2023-06-22 07:01:11 +0300

    Lesson 1 (#1)
    * task 1
    
    * task 2
    
    * task 3
    
    * task 4
    
    * task 5
    
    * task 6

--- task_2.py
+++ task_2.py
@@ -3,3 +3,9 @@
 в последовательность кодов (не используя методы encode и decode) и определить тип,
 содержимое и длину соответствующих переменных.
 """
+list_byte = [b'class', b'function', b'method']
+
+for i in list_byte:
+    print(type(i))
+    print(i)
+    print(len(i))
